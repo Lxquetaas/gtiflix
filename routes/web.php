9344e5bb3dbd532cdbbd0c442f04fd3d4752f67d
Lxquetaas <lucas.o.terto@gmail.com>
2025-09-18 10:46:27 -0300

    Aula 18-09

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/lucas', function (){
+   echo 'Olá Lucas';
+});
+
+Route::get('/filmes/{filme}', function ($filme){
+    echo 'Acessando dados do filme: ' .$filme;
+ });
+
+ Route::view('/sobre','sobre');
+ 
